43340f49c380f17cc608eec5a13cf9f9d695d6a8
alimaslax <alimaslax@gmail.com>
2023-02-26 18:34:52 -0500

    feat: saving work

--- scripts/youtube/yt.py
+++ scripts/youtube/yt.py
@@ -1,35 +1,50 @@
-from pytube import YouTube
+
 import asyncio
 import time
 import requests
 import ffmpeg
 import os
-
+import subprocess
+import sys
+from pytube import YouTube
 
 async def download(video_url):
     show_progress = True
     yt = YouTube(video_url, on_progress_callback=show_progress)
-    stream = yt.streams.filter(only_audio=True).first()
-    ffmpeg.input(stream.url, ss=0, t=yt.length).output('/data/output/{}.mp4'.format(yt.title)).run()
-    print('length', yt.length)
-    print("Download finished")
+    stream = yt.streams.filter().order_by('abr').desc().first()
+    # set the output to stdout
+    output = subprocess.PIPE
+    # download the stream to stdout
+    subprocess.run([stream.download()], stdout=output, shell=True)
+    audio_type = stream.mime_type
+    return audio_type
 
-
-async def cli():
+async def cli(links):
     print(f"started at {time.strftime('%X')}")
-    # get the current directory
-    current_dir = os.getcwd()
 
-    # create the full path for the links.txt file
-    links_file_path = os.path.join("/data/scripts/youtube", 'links.txt')
+    # loop through the links and download the videos
+    for link in links.split(','):
+        return await download(link.strip())
 
-    # read the links from the links.txt file and download the videos
-    with open(links_file_path, 'r') as f:
-        for line in f:
-            await download(line.strip())
+def app(environ, start_response):
+    # Get the request payload
+    length = int(environ.get('CONTENT_LENGTH', '0'))
 
-def main():
-    asyncio.run(cli())
+    links = environ.get('QUERY_STRING').split('=', 1)[1]
+    print(links)
 
-if __name__ == '__main__':
-    main()
+    # Run the main function
+    audio_type = main(links)
+    status = '200 OK'
+    headers = [('Content-type', audio_type)]
+    start_response(status, headers)
+
+    # Return the contents of the WAV file as the response body
+    output = subprocess.PIPE
+    return [output.stdout.decode()]
+
+def main(links):
+    return asyncio.run(cli(links))
+
+
+asyncio.run(cli('https://www.youtube.com/watch?v=w07DANgC6wA'))
